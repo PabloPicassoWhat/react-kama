884e850ce5ed54d2ba42c9f4d5717fedb30a0e1b
PabloPicassoWhat <magnimunin@gmail.com>
2022-06-13 20:19:12 +0300

    first commit

---
+++ src/components/Profile/MyPosts/Post/Post.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import s from './Post.module.css';
+
+const Post = (props) => {
+  return (
+    <div className={s.item}>
+      <img src='https://www.researchgate.net/publication/353422038/figure/fig2/AS:1048906112700416@1627090113328/Image-7-4-3-2-5-9-3-11-45-3-0-2-4-0-1-7.ppm' />
+      {props.message}
+      <div>
+        <span>like</span> {props.likesCounts}
+      </div>
+    </div>
+  );
+}
+
+export default Post;
---
+++ src/redux/dialogs-reducer.js
@@ -0,0 +1,45 @@
+const SEND_MESSAGE = 'SEND-MESSAGE';
+const UPDATE_NEW_MESSAGE_BODY = 'UPDATE-NEW-MESSAGE-BODY';
+
+let initialState = {
+  dialogs: [
+    {id: 1, name: 'Dimych'},
+    {id: 2, name: 'Andrey'},
+    {id: 3, name: 'Sveta'},
+    {id: 4, name: 'Sasha'},
+    {id: 5, name: 'Viktor'},
+    {id: 6, name: 'Valera'}
+  ],
+  messages: [
+    {id: 1, message: 'Hi'},
+    {id: 2, message: 'How is your it-kamasutra?'},
+    {id: 3, message: 'Yo'},
+    {id: 4, message: 'Yo'},
+    {id: 5, message: 'Yo'}
+  ],
+  newMessageBody: ''
+};
+
+const dialogsReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case SEND_MESSAGE:
+      let body = state.newMessageBody;
+      return {
+        ...state,
+        newMessageBody: '',
+        messages: [...state.messages, {id: 6, message: body}]
+      };
+    case UPDATE_NEW_MESSAGE_BODY:
+      return  {
+        ...state,
+        newMessageBody: action.body
+      };
+    default:
+      return state;
+  }
+}
+
+export const sendMessageCreator = () => ({type: SEND_MESSAGE});
+export const updateNewMessageBodyCreator = (body) => ({type: UPDATE_NEW_MESSAGE_BODY, body: body});
+
+export default dialogsReducer;
