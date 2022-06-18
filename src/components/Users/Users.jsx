b36743bb0f69f9dbb575eb5e3c562df89a7fc370
PabloPicassoWhat <magnimunin@gmail.com>
2022-06-18 21:15:31 +0300

    add preloader

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -1,35 +1,38 @@
 import React from "react";
-import styles from './Users.module.css'
-import * as axios from "axios";
-import userPhoto from "../../assets/images/profile-picture.png"
+import styles from "./Users.module.css";
+import userPhoto from "../../assets/images/profile-picture.png";
 
-class Users extends React.Component {
 
-  constructor(props) {
-    super(props);
+const Users = (props) => {
 
-      axios.get("https://social-network.samuraijs.com/api/1.0/users")
-        .then(response => {
-          this.props.setUsers(response.data.items)
-        });
+  let pagesCount = Math.ceil(props.totalUsersCount / props.pageSize);
+
+  let pages = [];
+  for (let i = 1; i <= pagesCount; i++) {
+    pages.push(i);
+    if(i === 20) break;
   }
 
-  render() {
-    return (
+  return (
+    <div>
       <div>
-        {
-          this.props.users.map(u => <div key={u.id}>
+        {pages.map( p => {
+          return <span className={props.currentPage === p && styles.selectedPage } onClick={ (e) => { props.onPageChanged(p) }}>{p}</span>
+        })}
+      </div>
+      {
+        props.users.map(u => <div key={u.id}>
           <span>
             <div>
-              <img src={u.photos.small !== null ? u.photos.small : userPhoto} className={styles.userPhoto}/>
+              <img src={u.photos.small !== null ? u.photos.small : userPhoto} className={styles.userPhoto} alt={`avatar`}/>
             </div>
             <div>
               { u.followed
-                ? <button onClick={ () => {this.props.unfollow(u.id)} }>Unfollow</button>
-                : <button onClick={ () => {this.props.follow(u.id)} }>Follow</button> }
+                ? <button onClick={ () => {props.unfollow(u.id)} }>Unfollow</button>
+                : <button onClick={ () => {props.follow(u.id)} }>Follow</button> }
             </div>
           </span>
-            <span>
+          <span>
             <span>
               <div>{u.name}</div>
               <div>{u.status}</div>
@@ -39,12 +42,10 @@ class Users extends React.Component {
               <div>{"u.location.city"}</div>
             </span>
           </span>
-          </div>)
-        }
-      </div>
-    );
-  }
-
+        </div>)
+      }
+    </div>
+  );
 }
 
 export default Users;
